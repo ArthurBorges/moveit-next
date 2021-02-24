3834560902c8aefe2d322528f5dafd74f1fe270c
ARTHUR LUIZ DA SILVA BORGES <arthur_lb@hotmail.com>
2021-02-24 15:13:49 -0300

    componente ChallengeBox Finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Head from 'next/head';
 import { CompletedChallenges } from "../components/CompleteChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-
-import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from '../styles/pages/Home.module.css';
+
 
 export default function Home() {
   return (
@@ -24,7 +25,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
