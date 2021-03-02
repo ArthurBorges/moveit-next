b8b2bf6cbe122f30b85dffc0ef623682ba6d7265
ARTHUR LUIZ DA SILVA BORGES <arthur_lb@hotmail.com>
2021-03-02 18:43:16 -0300

    Update ChallengeBox.tsx

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -37,7 +37,7 @@ export function ChallengeBox(){
                 </div>
             ) : (
                 <div className={styles.challengeNotActive}>
-                <strong>finalize um ciclo para receber um desafio</strong>
+                <strong>Finalize um ciclo para receber um desafio</strong>
                 <p>
                     <img src="icons/level-up.svg" alt="Level Up"/>
                     Avance de level completando desafios
